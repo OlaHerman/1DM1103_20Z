8b5b6edd47f95f83e2b582eb08703e8bd411e915
OlaHerman <73289850+OlaHerman@users.noreply.github.com>
2020-10-24 20:20:53 +0200

    Update zad1.c

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// argv[0] - nazwa programu
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     float v; 
     float a1;
-    a1= 5;
+    a1= 3;
     v = a1 * a1;
-    printf("[%f] wynik: %f\n", a1, v);
-    //printf("[%s] Suma: %f\n", argv[0]), v);
+    printf("Wartość [%f] do kwadratu to %f\n", a1, v);
 }
