6c509cf2c3ccf07b3113a15ba728b47e9604f297
OlaHerman <73289850+OlaHerman@users.noreply.github.com>
2020-11-22 21:58:29 +0100

    Update zad5_studenci.h

--- sem7/zadanie domowe/zad5_studenci.h
+++ sem7/zadanie domowe/zad5_studenci.h
@@ -3,22 +3,11 @@
 
 #include "zad5_dziekanat.h"
 
+
 int wczytaj(student dane[100], char *fnazwa);
 void wypisz(student dane[100], int n) ;
 int znajdz_s(char *szukany_nr, char nr_albumow[100][10], int n);
 int znajdz_studentow(char nr_albumow[100][10], student dane[100], int n);
-int znajdz_p(char *szukany_nr, char kod_przed[100][10], int n);
-int znajdz_przedmioty(char kod_przed[100][10], student dane[100], int n);
 
-typedef struct _student 
-{
-    char imie[25];
-    char nazwisko[50];
-    char nr_albumu[10];
-    char kod_przed[10];
-    char nazwa_przed[255];
-    float ocena;
-    int ects;
-} student, *pstudent;
 
 #endif
